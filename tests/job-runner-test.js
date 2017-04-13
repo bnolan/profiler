cd49cbc685b66b020d07274c86c0bac4e1d393b6
Ben Nolan <bnolan@gmail.com>
2017-04-13 12:53:20 +1200

    Add tests for job runner and linter.

---
+++ tests/job-runner-test.js
@@ -0,0 +1,64 @@
+var test = require('tape');
+var runner = require('../job-runner');
+
+test('start', (t) => {
+  runner.start();
+  t.end();
+});
+
+test('task starts async', (t) => {
+  t.plan(4);
+  
+  var first = true;
+
+  runner.enqueue((done) => {
+    t.ok(!first);
+
+    done();
+
+    t.ok(runner.isIdle())
+    t.ok(runner.isEmpty())
+  });
+
+  t.ok(first);
+  first = false;
+});
+
+test('three tasks happen in order', (t) => {
+  t.plan(6);
+  
+  var i = 1;
+
+  runner.enqueue((done) => {
+    t.equal(i, 1);
+    i++;
+
+    done();
+
+    t.ok(!runner.isEmpty());
+  });
+
+  runner.enqueue((done) => {
+    t.equal(i, 2);
+    i++;
+
+    done();
+
+    t.ok(!runner.isEmpty());
+  });
+
+  runner.enqueue((done) => {
+    t.equal(i, 3);
+    i++;
+
+    done();
+
+    t.ok(runner.isEmpty());
+  });
+});
+
+test('stop', (t) => {
+  runner.stop();
+  t.end();
+});
+
